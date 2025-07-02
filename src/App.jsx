c9e62bc5c9fe5fda7cac775dd2c48d7ea3ffb945
FaridOrtiz7K <farolinfeo135@gmail.com>
2025-07-02 13:54:43 -0600

    Creacion de las Secciones
    Se creo un nuevo componente donde se podran agragar o modificar mas secciones a toda la pagina principal sin que se agregue tanto codigo , mas que nada es la reutilización de este

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,61 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Navbar from './components/Navbar';//importar NavBar
+import Footer from './components/Footer';//importar SectionsGrid
+import { SectionsGrid } from './components/SectionCard'; //importar SectionsGrid
 
 const App = () => {
+  // Mueve la declaración de sectionsData fuera del JSX
+  const sectionsData = [
+    {
+      title: "Hidalgo en mapas",
+      description: "Mapas interactivos de interés ciudadano, gastronomía, recreación, cultura, entre otros temas",
+      imageUrl: "./images/sec/1.png",
+      linkUrl: "./pags/h_mapas.php"
+    },
+    {
+      title: "Hidalgo en números",
+      description: "Información relevante y sintetizada en tableros dinámicos e infografías por municipio",
+      imageUrl: "./images/sec/2.png",
+      linkUrl: "./pags/h_numeros.php"
+    },
+     {
+      title: "Hidalgo en números",
+      description: "Información relevante y sintetizada en tableros dinámicos e infografías por municipio",
+      imageUrl: "./images/sec/2.png",
+      linkUrl: "./pags/h_numeros.php"
+    },
+     {
+      title: "Hidalgo en números",
+      description: "Información relevante y sintetizada en tableros dinámicos e infografías por municipio",
+      imageUrl: "./images/sec/2.png",
+      linkUrl: "./pags/h_numeros.php"
+    },
+     {
+      title: "Hidalgo en números",
+      description: "Información relevante y sintetizada en tableros dinámicos e infografías por municipio",
+      imageUrl: "./images/sec/2.png",
+      linkUrl: "./pags/h_numeros.php"
+    },
+     {
+      title: "Hidalgo en números",
+      description: "Información relevante y sintetizada en tableros dinámicos e infografías por municipio",
+      imageUrl: "./images/sec/2.png",
+      linkUrl: "./pags/h_numeros.php"
+    },
+    
+    // ... puedes agregar más secciones aquí
+  ];
+
   return (
     <div className="app">
       <Navbar />
-      
-      {/* Aquí iría el contenido principal de tu aplicación */}
+
+      {/* Contenido principal */}
       <main>
-        {/* ... */}
+        <h1>HOLAAAA PUTA kfdnkjnsfdkjnksnfknskfdnnfflnjafnjkanfjkn</h1>
+        <SectionsGrid sections={sectionsData} />
       </main>
-      
+
       <Footer />
     </div>
   );
