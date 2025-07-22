5b0a24d98cdb1464c32290c34209282c0fcd3eba
Metzli <143217861+m3tzly@users.noreply.github.com>
2025-07-22 10:24:35 -0600

    Cambio de las path
    se cambio la path para que sea mas claro,

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import Home from '../src/pages /Home';
+import Home from '../src/pages/home';
 import h_mapas from './pages/h_mapas/h_mapas';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
